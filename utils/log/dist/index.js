529497cebe812fe664edca0b4d7983179a3e3ec7
YDD <792196835@qq.com>
2021-01-30 22:48:27 +0800

    a

--- utils/log/dist/index.js
+++ utils/log/dist/index.js
@@ -1,10 +1,10 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const log = require("npmlog");
+exports.__esModule = true;
+var log = require("npmlog");
 // 配置loginfo等级
 // log.level = process.env.LOG_LEVEL ? process.env.LOG_LEVEL : "info";
 // 自定义log样式
 // log.heading = "dd-cli";
 // log.headingStyle = { fg: "red", bg: "white" };
-log.addLevel("sucess", 2000, { fg: "green" });
-exports.default = log;
+log.addLevel("sucess", 2001, { fg: "green" });
+exports["default"] = log;
